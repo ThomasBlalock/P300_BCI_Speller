f0db6aeb82265fa3c5fd90aea143b41476a2d656
ThomasBlalock <blalockthomasm@gmail.com>
2023-10-27 19:24:28 -0600

    added DataHandler, DataObject, and DataVisitors code to the lib of the main program.. outside of the 'experiments' section

---
+++ lib/DataVisitors.py
@@ -0,0 +1,62 @@
+class DataVisitor(object):
+
+    @staticmethod
+    def visit_data_object(object):
+        raise NotImplementedError
+    
+    @staticmethod
+    def visit_session_data(session):
+        raise NotImplementedError
+    
+    @staticmethod
+    def visit_trial_data(trial, raw_data):
+        raise NotImplementedError
+
+
+##########################################################################################
+
+
+class MakeWindowsDataVisitor(DataVisitor):
+    """
+    Returns a list of tuples ( data_window, label )
+    """
+
+
+    @staticmethod
+    def visit_data_object(object, type):
+        """
+        Returns a list of tuples ( data_windows, label )
+        """
+        sessions = object.keyboard_sessions if type == 'keyboard' else object.box_sessions
+
+        data = []
+        for session in sessions:
+            data.extend(session.get_data(visitor=MakeWindowsDataVisitor()))
+        return data
+    
+
+    @staticmethod
+    def visit_session_data(session):
+        """
+        Returns a list of tuples ( data_windows, label )
+        """
+        data = []
+        for trial in session.trials:
+            data.append(trial.get_data(visitor=MakeWindowsDataVisitor()))
+        return data
+    
+
+    @staticmethod
+    def visit_trial_data(trial):
+        """
+        Returns a tuple ( data_windows, label )
+        """
+        start = trial.timestamp[0]
+        end = start + trial.parent_session.sample_time
+
+        # Make it an index for the data rather than a time
+        data_len = len(trial.parent_session.data[0])
+        start = (int) ( (start / trial.parent_session.length) * data_len )
+        end = (int) ( (end / trial.parent_session.length) * data_len )
+
+        return ( trial.parent_session.data[:, start:end], trial.label )
