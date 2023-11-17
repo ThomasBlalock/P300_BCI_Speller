9972d875c8508898c6ee0e6ea6a5da3b9c9eb92b
ThomasBlalock <blalockthomasm@gmail.com>
2023-11-17 11:40:41 -0700

    model_training file all debugged

--- lib/utils.py
+++ lib/utils.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+import torch
 
 
 def load_data(directory):
@@ -18,3 +19,26 @@ def load_data(directory):
       files.append(pickle.load(f))
 
   return files
+
+
+def train(dataloader, model, loss_fn, optimizer, print_batch=False):
+    size = len(dataloader.dataset)
+    iter_num = len(dataloader)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0 and print_batch:
+            loss, current = loss.item(), (batch+1) * len(X)
+            accuracy = (pred.argmax(1) == y).type(torch.float).sum().item() / len(y)
+            print(f'Batch {batch+1} / {iter_num} ~ loss: {loss:>7f} ~  accuracy: {accuracy:>7f}')
+    if print_batch:
+        loss, current = loss.item(), (batch+1) * len(X)
+        accuracy = (pred.argmax(1) == y).type(torch.float).sum().item() / len(y)
+        print(f'Batch {batch+1} / {iter_num} ~ loss: {loss:>7f} ~  accuracy: {accuracy:>7f}')
